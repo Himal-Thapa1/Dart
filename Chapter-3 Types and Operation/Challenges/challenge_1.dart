f10a42a87f7f2353b62a80311db3c37a853e4df5
Himal Thapa <92637955+Himal-Thapa1@users.noreply.github.com>
2022-08-18 17:16:41 +0545

    Add files via upload

---
+++ Chapter-3 Types and Operation/Challenges/challenge_1.dart
@@ -0,0 +1,17 @@
+// Challenge 1: Teacher’s grading
+// You’re a teacher, and in your class, attendance is worth 20%
+// of the grade, the homework is worth 30% and the exam is
+// worth 50%. Your student got 90 points for her attendance, 80
+// points for her homework and 94 points on her exam.
+// Calculate her grade as an integer percentage rounded down.
+
+
+
+void main(){
+  int attendance=90;
+  int homework=80;
+  int exam= 94;
+  int totalPercentage;
+  totalPercentage=((attendance*0.2)+(homework*0.3)+(exam*0.5)).toInt();
+  print("The total percentage the student got it $totalPercentage %.");
+}
---
+++ Chapter-3 Types and Operation/mini Excercises/mini_1.dart
@@ -0,0 +1,10 @@
+//  Create a constant called age1 and set it equal to 42.
+// Create another constant called age2 and set it equal to
+// 21. Check that the type for both constants has been
+// inferred correctly as int by hovering your mouse pointer
+// over the variable names in VS Code.
+void main(){
+  const age1=42;
+  const age2=21;
+
+}
